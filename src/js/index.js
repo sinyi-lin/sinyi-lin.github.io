029e75b0accfa0ee4effb526ddb5cf4ed4175485
Sinyi <buns3366@gmail.com>
2020-10-19 23:22:40 +0800

    add: web page

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// import '.././sass/bootstrap-reset-breakpoint.sass';
+// import '.././sass/layout.sass';
+// import '.././sass/style.sass';
+// import '.././sass/index.sass';
+
+
+// console.log("hello webpack!!!!幫我更新吧！");
+
